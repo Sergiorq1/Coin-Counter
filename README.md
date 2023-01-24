732c27a9239669405391c7d53db0018377fa650d
Sergiorq1 <sergiorq12016@gmail.com>
2023-01-23 20:47:50 -0700

    init react project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Coin-Counter
+
+## React App that counts coins
+
+### In efforts to better understand useState hook
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Head'>
+        <h1>Coin Collector</h1>
+        <h2>Total: 100</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
